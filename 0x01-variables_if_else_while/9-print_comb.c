36cfc9309c65addaab856bd9772e6ee3bf478bfa
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-06-17 09:41:03 -0700

    Finally, i'm done i think

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,4 +10,19 @@
 int  main(void)
 
 {
-	int 
+	int d;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
