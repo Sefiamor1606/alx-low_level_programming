d08d0fc5ae19b782e9a3442125b83ba5ddf932b0
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-06-22 16:24:12 -0700

    Numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9
- * Description: prits from 0 o0 9 excluding 2 and 4
- * Return numbers from 0 to 9
+ * print_most_numbers -  prints the numbers from 0 to 9 excluding 2 and 4
+ * Description: Prints excluding 2 and 4
+ * Return: numbers from 0 to 9 exclude 2 and 4
  */
 
 void print_most_numbers(void)
 {
+    int x;
 
-	int x;
-
-	for (x = 0; x <= 9 x++);
-	{	
-	if (x == 2 || x == 4);
-	{
-		continue;
-	}
-	else
-	{
-	_putchar(x +'0');
-	}
-	}
-	_putchar('\n');
+    for (x = 0; x <= 9; x++)
+    {
+        if (x == 2 || x == 4)
+        {
+            continue;
+        }
+        else
+        {
+            _putchar(x + '0');
+        }
+    }
+    _putchar('\n');
 }
+
