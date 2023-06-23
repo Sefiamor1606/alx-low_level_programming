dad785ba7036f356f846e4939e74937236d62520
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-06-23 16:18:01 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program will assign a random number to the variable n
+ * each time it is executed.
+ * Return: Always (0)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("The number is: %d\n", n);
+
+	if (n > 0)
+	{
+	printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+	printf("is zero\n");
+	}
+	else
+	{
+	printf("is negative\n");
+	}
+
+	return (0);
+}
+
