fa10ae2a2bdcfd1930d67a9d4da008d4e7a61567
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-06-26 13:49:00 -0700

    Battery st

--- 0x03-debugging/100-prime_factor.c
+++ 0x03-debugging/100-prime_factor.c
@@ -1,25 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 /**
- * main -  prints the largest prime factor of the number 612852475143
+ * main - prints the largest prime factor of the number 612852475143
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	long x, maxf;
-	long number = 612852475143;
-	double square = sqrt(number);
+    long number = 612852475143;
+    long largestFactor = 1;
+    long square = sqrt(number);
 
-	for (x =1; x <= square; x++)
-	{
-		if (number % x == 0)
-		{
-			maxf = number / x;
-		}
-	}
-	printf("%ld\n", maxf);
-	return (0);
+    for (long i = 2; i <= square; i++)
+    {
+        if (number % i == 0)
+        {
+            if (is_prime(i))
+                largestFactor = i;
+            if (is_prime(number / i))
+                largestFactor = number / i;
+        }
+    }
+
+    printf("The largest prime factor of %ld is %ld\n", number, largestFactor);
+
+    return 0;
 }
+
