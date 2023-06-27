f8b892a5f4f59bdbb1b6ac5503fb2cd902a10506
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-06-26 19:48:05 -0700

    Half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * puts2 - prints every other character of a string, starting with the first character
@@ -12,16 +13,16 @@ void puts2(char *str)
 	int i;
 	int j = 0;
 
-	while (str[j] != "/0")
+	while (str[j] != '\0')
 	{
 	j++;
 	}
 
 	for (i = 0; i < j; i += 2)
 	{
-	_putchar(str[i]);
+	putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 
 }
 
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
 
 /**
  * _strcpy - Copies a string pointed to by src,
