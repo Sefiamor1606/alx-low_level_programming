97de34f585c49cc08ba92dc84de68f4cda2dea26
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-07-03 07:23:02 -0700

    Task 6 and Chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Rteurn: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 0; i++)
+	{
+	for (n = 0; n < 0; n++)
+	putchar(a[i][n]);
+	putchar('\n');
+	}
+}
