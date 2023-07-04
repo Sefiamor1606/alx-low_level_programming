f2985206a49751aa024b3b2a6681d35247619d8d
Sefiamor1606 <svkomla.abotsi@gmail.com>
2023-07-04 04:39:41 -0700

    Task 6 print and chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,13 +8,16 @@
  */
 
 void print_chessboard(char (*a)[8])
-{
-	int i, n;
 
-	for (i = 0; i < 0; i++)
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
-	for (n = 0; n < 0; n++)
-	putchar(a[i][n]);
+	for (j = 0; j < 8; j++)
+	{
+	putchar(a[i][j]);
+	}
 	putchar('\n');
 	}
 }
